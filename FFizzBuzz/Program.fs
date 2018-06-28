371261fa7d1be934e6bf276653f0a32d5d4b2d06
MADDY\daeer <daeer73@gmail.com>
2018-06-28 20:23:59 +0100

    Initial commit of the F# version of the FizzBuzz game.

---
+++ FFizzBuzz/Program.fs
@@ -0,0 +1,27 @@
+﻿// Learn more about F# at http://fsharp.org
+// See the 'F# Tutorial' project for more help.
+
+let fizzBuzz number = 
+    match number with
+    | _ when number % 15 = 0 -> "fizzbuzz"
+    | _ when number % 3 = 0 -> "fizz"
+    | _ when number % 5 = 0 -> "buzz"
+    | _ -> string number
+
+
+[<EntryPoint>]
+let main argv = 
+
+    let stopWatch = new System.Diagnostics.Stopwatch()
+
+    stopWatch.Start()
+
+    for i in [1..1000] do
+        printfn "%s" (fizzBuzz i)
+
+    stopWatch.Stop()
+    printfn "Elapsed Time: %i" stopWatch.ElapsedMilliseconds
+
+    printfn "Press any key to continue."
+    System.Console.ReadKey() |> ignore
+    0 // return an integer exit code
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # FizzBuzzGame_FSharp
 An example of the FizzBuzz game written using F#.
+
+This was one of my first attempts to use F#.
+
+I have done some comparisons of performance between the C# version and this F# version.
+
+I found that the C# version was faster, although, not by much.  The bonus of F#, is the amount of code written.
+
+I am looking at doing different things with F#, so see if I can find out what code or patterns that can be used to show F# is faster than C#.
